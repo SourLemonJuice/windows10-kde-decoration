283c8700cbee0e242e907086668c4b408a810df1
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-02-13 07:48:37 +0800

    添加 install.sh 脚本

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+theme_name="Windows10-kde"
+
+aurorae_dir="${HOME}/.local/share/aurorae/themes"
+
+test ! -d "${aurorae_dir}" && mkdir -p "${aurorae_dir}"
+test -d "${aurorae_dir}/${theme_name}" && rm -r "${aurorae_dir}/${theme_name}"
+
+function install-aurorae {
+    echo "Copying themes to ${aurorae_dir}"
+    cp -r aurorae/themes/* "${aurorae_dir}"
+}
+
+install-aurorae
+echo "Install aurorae decoration finished"
