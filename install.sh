08bdbe80d097752609bd1725f39568de9562b4e2
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-11-02 11:21:46 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-theme_name="Windows10-kde"
+set -euo pipefail
 
 aurorae_dir="${HOME}/.local/share/aurorae/themes"
 
 test ! -d "${aurorae_dir}" && mkdir -p "${aurorae_dir}"
-test -d "${aurorae_dir}/${theme_name}" && rm -r "${aurorae_dir}/${theme_name}"
 
-function install-aurorae {
-    echo "==> Copying themes to ${aurorae_dir}"
-    cp --verbose -R aurorae/themes/* "${aurorae_dir}"
-}
-
-install-aurorae
+echo "==> Copying themes to ${aurorae_dir}"
+cp --verbose -R aurorae/themes/* "${aurorae_dir}"
 echo "==> Install aurorae decoration finished"
